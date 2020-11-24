502ebca89b407aa1f37ec3f00e6b33f07474827a
ozzwozz <61410630+ozzwozz@users.noreply.github.com>
2020-11-24 23:11:49 +0000

    Library Adding

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include "mbed.h"
 #include <cstdio>
 #include "SDFileSystem.h"
+#include "F429_Mega_Shell_Header.h"
+
 
 #include "SD_Card.h"
 //SDFileSystem sd(p5, p6, p7, p8, "sd"); // the pinout on the mbed Cool Components workshop board
 
-thread t1; //Sensor Thread
+Thread t1; //Sensor Thread
 Thread t2; //Output Thread
 Thread t3; //Ethernet Thread
 Thread t4; //SD Card Thread
@@ -14,8 +16,8 @@ InterruptIn SD_Active();
 
 int main()
 {
-    SD_Active.rise(&SD_init);
-    SD_Active.fall(&SD_deinit);
+    SD_Active().rise(&SD_init);
+    SD_Active().fall(&SD_deinit);
     //t1.start(Sensor);
     //t2.start(Output);
     //t3.start(Ethernet);
